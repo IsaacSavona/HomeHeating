02588867fb829a6cb6eddbe22409940a2721d654
IsaacSavona <isaacsavona@gmail.com>
2024-10-14 16:47:06 +0200

    ran pre-commit tests and made appropraite changes

--- homeheating/FOPDT_model.py
+++ homeheating/FOPDT_model.py
@@ -1,24 +1,34 @@
+from random import uniform
+from heating_model import HeatingModel
 
-class FOPDTModel(HeatModel):
+
+class FOPDTModel(HeatingModel):
     def __init__(self):
-        self.__gain = random(-100,100)
-        self.__time_constant = random(0,100)
-        self.__dead_time = random(0,10)
+        self.__gain = uniform(-100, 100)
+        self.__time_constant = uniform(0, 100)
+        self.__dead_time = uniform(0, 10)
 
     def siso_process(self, y, t, u):
         Kp = self.__gain
         taup = self.__time_constant
         thetap = self.__dead_time
 
+        dydt = 0
+
         # calculate derivative
-        dydt[0] = (-y[0] + Kp * u)/(taup/n)
-        for i in range(1,n):
-            dydt[i] = (-y[i] + y[i-1])/(taup/n)
+        dydt = (-y + Kp * u(t - thetap)) / (taup)
+
         return dydt
+
+    @property
+    def dead_time(self):
+        return self.__dead_time
 
     @dead_time.setter
     def set_dead_time(self, dead_time):
-        if(dead_time < 0): 
-            raise ValueError("Sorry you can not have non-causal systems where an input affects a change in the past")
+        if dead_time < 0:
+            raise ValueError(
+                "Sorry you can not have non-causal systems where an input affects a change in the past"
+            )
 
         self.__dead_time = dead_time
--- homeheating/heating_model.py
+++ homeheating/heating_model.py
@@ -1,7 +1,8 @@
 class HeatingModel:
-    def __init__(self, model_type):
+    def __init__(
+        self, model_type, noise, constant_disturbance, time_varying_disturbance
+    ):
         self.model_type = model_type
         self.noise = noise
         self.constant_disturbance = constant_disturbance
         self.time_varying_disturbance = time_varying_disturbance
-
