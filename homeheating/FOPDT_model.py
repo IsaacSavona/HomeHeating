42ff62a02e3b06f0b0811a9a02779c38bd94614c
IsaacSavona <isaacsavona@gmail.com>
2024-10-14 17:06:42 +0200

    commenting FOPDT_model

--- homeheating/FOPDT_model.py
+++ homeheating/FOPDT_model.py
@@ -9,6 +9,18 @@ class FOPDTModel(HeatingModel):
         self.__dead_time = uniform(0, 10)
 
     def siso_process(self, y, t, u):
+        """Define the first-order-plus-dead-time model ode. This is to be used with the scipy function `odeint`
+
+        Args:
+            y: output of the system
+            t: time
+            u: input to the system
+
+        Returns:
+            dydt: the derivative of the ouput
+
+        """
+
         Kp = self.__gain
         taup = self.__time_constant
         thetap = self.__dead_time
@@ -16,7 +28,7 @@ class FOPDTModel(HeatingModel):
         dydt = 0
 
         # calculate derivative
-        dydt = (-y + Kp * u(t - thetap)) / (taup)
+        dydt = (-y + Kp * u * (t - thetap)) / (taup)
 
         return dydt
 
@@ -28,7 +40,7 @@ class FOPDTModel(HeatingModel):
     def set_dead_time(self, dead_time):
         if dead_time < 0:
             raise ValueError(
-                "Sorry you can not have non-causal systems where an input affects a change in the past"
+                "Sorry you cannot have non-causal systems where an input affects a change in the past"
             )
 
         self.__dead_time = dead_time
